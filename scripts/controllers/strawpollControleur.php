6cf085839d13a6d6cf81d93110bd5c0ea8c7a6e3
steven souppaya <ssouppaya@sqli.com>
2017-03-02 17:06:16 +0100

    Creation controleur strawpoll

---
+++ scripts/controllers/strawpollControleur.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ssouppaya
+ * Date: 02/03/17
+ * Time: 16:26
+ */
+class strawpollControleur
+{
+    public function executeRunStrawpoll()
+    {
+
+    }
+}
