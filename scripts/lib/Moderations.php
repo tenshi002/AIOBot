53d0fcdcb8d5f556ed26e89fcc9fa5a98865b8d3
steven <souppayasteven@gmail.com>
2017-07-28 02:47:37 +0200

    [update] mise à jour du filtre anti link et anti spam maj

--- scripts/lib/Moderations.php
+++ scripts/lib/Moderations.php
@@ -22,15 +22,55 @@ class Moderations
         //TODO récupérer la conf web du bot via un autre fichier ini ?
     }
 
+    /**
+     * @param $message string texte du stream à vérifier
+     * @return bool
+     */
     public function antiLink($message)
     {
-        $regexp = '^((http[s]?):\/\/)?([^:\/\s]+)\/([\w\-\.]+[^#?\s]+)?$';
-        $regexpClipTwitch = '^(www\.|http[s]?)(:\/\/)?(www\.)?(clips\.twitch\.tv)(\/)?([\w\-\.]+[^#?\s]+)?$';
-        if(preg_match('/' . $regexp . '/', $message))
-        {
+        //TODO à verifier et completer !
 
+        $regexp = '((http[s]?):\/\/)?([^:\/\s]+)\/([\w\-\.]+[^#?\s]+)?$';
+        $regexpClipTwitch = '^(www\.|http[s]?)(:\/\/)?(www\.)?(clips\.twitch\.tv)(\/)?([\w\-\.]+[^#?\s]+)?$';
+        // 1 - on verifie si le texte contient un lien clip twitch
+        $matchClip = preg_match('/' . $regexpClipTwitch . '/', $message);
+        if($matchClip === 1 && $matchClip !== null)
+        {
+            return false;
         }
 
+        // 2 - le texte ne contient pas la mention clip, on test s'il contient un lien
+        $matchLink = preg_match($regexp, $message);
+        if($matchLink === 1 && $matchLink !== null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function antiSpam($message)
+    {
+        // 1 - on traite le cas des majuscules :
+        $messagesArray = explode(' ', $message);
+        $fullStringUpper = true;
+        foreach($messagesArray as $word)
+        {
+            if(ctype_upper($word))
+            {
+                continue;
+            }
+            else
+            {
+                $fullStringUpper = false;
+                break;
+            }
+        }
+
+        if($fullStringUpper)
+        {
+            return true;
+        }
         return false;
     }
 
