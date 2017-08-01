4741db615eb7b94787675c3d1acd7d6f8e3c9102
Nicolas <nbadiasanmartin@sqli.com>
2017-08-01 17:52:42 +0200

    [work] init database roles

---
+++ database/init.sql
@@ -0,0 +1,8 @@
+INSERT INTO role
+  (id, nom)
+VALUES
+  (1, 'Administrator'),
+  (2, 'Editor'),
+  (3, 'Regular'),
+  (4, 'Viewer');
+
---
+++ scripts/lib/RPG/Stats.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: nico
+ * Date: 01/08/17
+ * Time: 16:06
+ */
+
+namespace lib\RPG;
+
+
+class Stats
+{
+    const LEVEL_MIN = 1;
+    const LEVEL_MAX = 100;
+
+    public function
+}
